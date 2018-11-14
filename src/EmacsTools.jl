b46261cc01e7fe5e2ac6e43b72bc16273c5a6d97
Nicolae Cîndea <nicolae.cindea@uca.fr>
2018-11-14 14:49:43 +0100

    Fix compatibilities for Julia 1.0

--- src/EmacsTools.jl
+++ src/EmacsTools.jl
@@ -1,21 +1,23 @@
 module EmacsTools
+using REPL
 
 export get_completions, get_latex_symbols
 
 # print completions of SUBSTRING
 function get_completions(substring::AbstractString)
-  c = Base.REPLCompletions.completions(substring, length(substring));
-  for completion in c[1]
-    println(completion);
-  end
+    c = REPL.REPLCompletions.completions(substring, length(substring));
+    for completion in c[1]
+        if isa(completion, REPL.REPLCompletions.ModuleCompletion)
+            println(completion.mod);
+        end
+    end
 end
 
 # print all available LaTeX symbols
 function get_latex_symbols()
-  c = Base.REPLCompletions.latex_symbols;
-  for (latex, unicode) in sort!(collect(c), by=x->x[2])
-    println("$latex $unicode")
-  end
+    c = REPL.REPLCompletions.latex_symbols;
+    for (latex, unicode) in sort!(collect(c), by=x->x[2])
+        println("$latex $unicode")
+    end
 end
-
 end # module
